f0c8c9cd17ac0b9346f4ffdeb90e7afe50c33e10
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-01-23 15:27:19 +0100

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,7 @@
+/* eslint-disable no-undef */
+module.exports = {
+  extends: 'react-native-matipl01',
+  rules: {
+    'import/no-unused-modules': 'off'
+  }
+};
---
+++ app/index.tsx
@@ -0,0 +1,5 @@
+import { View } from 'react-native';
+
+export default function HomeScreen() {
+  return <View style={{ backgroundColor: 'red', flex: 1 }} />;
+}
